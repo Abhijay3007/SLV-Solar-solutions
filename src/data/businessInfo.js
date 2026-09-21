74639222c0910061688d995de3b592ca2239db8a
Abhijay 3007 <abhijayr05@gmail.com>
2026-09-21 17:43:15 +0530

    Facebook link updated

--- src/data/businessInfo.js
+++ src/data/businessInfo.js
@@ -14,7 +14,7 @@ export const BUSINESS_INFO = {
   mapQueryUrl: 'https://maps.google.com/?q=Nelamangala,+Karnataka,+India',
   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62174.45501861073!2d77.35928738361735!3d13.098939316694632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae2f9746e8b7c7%3A0xd68d7122a27546a1!2sNelamangala%20Town%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin',
   instagramUrl: 'https://www.instagram.com/slvsolarasolutions?stkn=MXNqenN6czNwYXdrMg%3D%3D&utm_source=qr',
-  facebookUrl: 'https://www.facebook.com/share/1FWZvcobfQ/',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=61594486116843',
   whatsappNumber: '6362003541',
   whatsappUrl: 'https://wa.me/916362003541?text=Hi,%20I%27m%20interested%20in%20solar%20solutions%20from%20SLV%20Solar%20Solutions.'
 };
